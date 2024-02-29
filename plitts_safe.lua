83690dca5fd112032e601f4d910a202598099063
Durman96 <121059822+Durman96@users.noreply.github.com>
2024-02-29 08:52:38 +0300

    Update plitts_safe.lua

--- plitts_safe.lua
+++ plitts_safe.lua
@@ -1,6 +1,6 @@
 script_name("plitts_safe")
-script_version("0.1")
-
+script_version("0.2")
+-- 123123
 local enable_autoupdate = true -- false to disable auto-update + disable sending initial telemetry (server, moonloader version, script version, samp nickname, virtual volume serial number)
 local autoupdate_loaded = false
 local Update = nil
